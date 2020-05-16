3089a6d612f07cb9c15eda6231cb711f7b947e11
DarkDrek <wackerei@gmail.com>
2020-05-16 11:55:40 +0200

    Document use and usage of new macro

--- tests/type_check/fail/arithmetic/mod.rs
+++ tests/type_check/fail/arithmetic/mod.rs
@@ -1,5 +1,4 @@
-use crate::test_type;
-use crate::test_type_with_placeholder;
+use crate::{test_type, test_type_with_placeholder};
 use pijama::ty::{Ty, TyError};
 use pijama::LangError;
 
@@ -10,13 +9,7 @@ test_type!(
         found: Ty::Bool
     }))
 );
-test_type!(
-    wrong_type_sub,
-    Err(LangError::Ty(TyError::Mismatch {
-        expected: Ty::Int,
-        found: Ty::Bool
-    }))
-);
+
 test_type!(
     mixed_types_add,
     Err(LangError::Ty(TyError::Mismatch {
@@ -25,8 +18,9 @@ test_type!(
     }))
 );
 
+// Test all int binary operators with bool arguments
 test_type_with_placeholder!(
-    wrong_type_with_placeholder,
+    wrong_type_int_binop_with_placeholder,
     Err(LangError::Ty(TyError::Mismatch {
         expected: Ty::Int,
         found: Ty::Bool
--- tests/type_check/mod.rs
+++ tests/type_check/mod.rs
@@ -10,6 +10,8 @@ fn type_check(input: &str) -> LangResult<Ty> {
     Ok(ty::ty_check(&mir)?)
 }
 
+/// Create a test with `$name` that type checks a file with `$name`.pj
+/// in the same directory against the `$pattern`.
 #[macro_export]
 macro_rules! test_type {
     ($name:ident, $pattern:pat) => {
@@ -22,9 +24,12 @@ macro_rules! test_type {
     };
 }
 
+/// Create a test with `$name` that type checks a file with `$name`.pj
+/// in the same directory against the `$pattern`. It does this once for
+/// each `$replacement` that will replace the `$placeholder` in the file.
 #[macro_export]
 macro_rules! test_type_with_placeholder {
-    ($name:ident, $pattern:pat, $placeholder:tt, $( $replacement:tt ),*) => {
+    ($name:ident, $pattern:pat, $placeholder:tt, $( $replacement:tt ),+) => {
         #[test]
         fn $name() {
             let input = include_str!(concat!(stringify!($name), ".pj"));
@@ -36,7 +41,9 @@ macro_rules! test_type_with_placeholder {
             for replacement in &replacements {
                 let input = input.replace(stringify!($placeholder), replacement);
                 let ty = crate::type_check::type_check(&input);
-                assert!(matches!(ty, $pattern), "failed with replacement {}\n{:#?}", replacement, ty);
+                assert!(matches!(ty, $pattern),
+                    "failed with replacement {}\n{:#?}",
+                    replacement, ty);
             }
         }
     };
