f85f195ad5b191b2498e6f5beac68119f6cd7949
Christian Poveda <git@christianpoveda.xyz>
2020-05-17 19:16:16 -0500

    move comments about spaces for blocks to each parser

--- src/parser/block.rs
+++ src/parser/block.rs
@@ -1,6 +1,8 @@
 //! Parsers for blocks.
 //!
-//! Blocks are sequences of nodes separated by at least one line break and optional spaces.
+//! The [`block0`] parser is the top-level parser of the whole [`parser`] module.
+//!
+//! [`parser`]: crate::parser
 
 use nom::{error::ParseError, IResult};
 
@@ -15,11 +17,15 @@ use crate::ast::Block;
 use crate::parser::node::node;
 
 /// Parser for [`Block`]s.
+///
+/// Nodes in the block can be separated by at least one line break and optional spaces.
 pub fn block0<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, Block, E> {
     separated_list(line_ending, preceded(multispace0, node))(input)
 }
 
 /// Parser for non-empty [`Block`]s.
+///
+/// Nodes in the block can be separated by at least one line break and optional spaces.
 pub fn block1<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, Block, E> {
     separated_nonempty_list(line_ending, preceded(multispace0, node))(input)
 }
