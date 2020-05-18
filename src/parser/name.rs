bbd39559a3470c30d4aad254506f5fdd4c516b61
Christian Poveda <git@christianpoveda.xyz>
2020-05-17 19:28:00 -0500

    add comment about zero-copy parsing for names

--- src/parser/name.rs
+++ src/parser/name.rs
@@ -1,28 +1,28 @@
 //! Parsers for names.
 //!
-//! The entry-point for this module is the [`name`] function. Names of variables in Pijama must be
+//! The entry point for this module is the [`name`] function. Names of variables in Pijama must be
 //! alphabetic `snake-case` strings. Certain keywords such as `fn`, `do` and `end` cannot be names,
 //! such keywords are in the [`KEYWORDS`] constant.
-//!
-//! [`name`]: crate::parser::name::name.
-//! [`KEYWORDS`]: crate::parser::name::KEYWORDS.
-//! [`Name`]: crate::ast::Name
 use nom::{error::ParseError, IResult};
 
 use nom::{
     character::complete::{alpha1, char},
     combinator::{map, recognize, verify},
-    multi::separated_nonempty_list
+    multi::separated_nonempty_list,
 };
 
 use crate::ast::Name;
 
 /// Words that cannot be names to avoid ambiguities.
 const KEYWORDS: &[&str] = &[
-    "fn", "do", "end", "if", "else", "true", "false", "unit", "Bool", "Int", "Unit", "rec",
+    "fn", "rec", "do", "end", "if", "else", "true", "false", "unit", "Bool", "Int", "Unit",
 ];
 
 /// Parser for [`Name`]s.
+///
+/// This parser is the main reason why most of the types and functions in the language are generic
+/// over the `'a` lifetime. It allows to do zero-copy parsing and keep using the string slices
+/// for the names through all the compilation process.
 pub fn name<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, Name<'a>, E> {
     map(
         verify(recognize(separated_nonempty_list(char('_'), alpha1)), |s| {
