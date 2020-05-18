e1557a5301513d1e79d9b0bcb4e49541a6a9072b
Christian Poveda <git@christianpoveda.xyz>
2020-05-17 22:14:55 -0500

    add support for anonymous functions

--- tests/type_check/fail/functions/mod.rs
+++ tests/type_check/fail/functions/mod.rs
@@ -17,3 +17,19 @@ test_type!(
         found: Ty::Arrow(box Ty::Int, box Ty::Int),
     }))
 );
+
+test_type!(
+    wrong_type_anon_fn_call_arg,
+    Err(LangError::Ty(TyError::Mismatch {
+        expected: Ty::Int,
+        found: Ty::Bool
+    }))
+);
+
+test_type!(
+    wrong_return_type_anon_fn_int_to_int,
+    Err(LangError::Ty(TyError::Mismatch {
+        expected: Ty::Arrow(box Ty::Int, box Ty::Bool),
+        found: Ty::Arrow(box Ty::Int, box Ty::Int),
+    }))
+);
