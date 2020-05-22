2f20422c0c6077c78076399783f1432fe74f0609
Christian Poveda <git@christianpoveda.xyz>
2020-05-22 12:14:08 -0500

    add functions to type each term

--- src/ty/ty_check.rs
+++ src/ty/ty_check.rs
@@ -1,5 +1,5 @@
 use crate::{
-    ast::{BinOp, Literal, Located, Primitive, UnOp},
+    ast::{BinOp, Literal, Located, Location, Name, Primitive, UnOp},
     mir::Term,
     ty::{Binding, Ty, TyError, TyResult},
 };
@@ -34,113 +34,188 @@ struct Context<'a> {
 impl<'a> Context<'a> {
     fn type_of(&mut self, term: &Located<Term<'a>>) -> TyResult<Located<Ty>> {
         let loc = term.loc;
-        let ty = match &term.content {
-            Term::Lit(lit) => match lit {
-                Literal::Unit => Ty::Unit,
-                Literal::Bool(_) => Ty::Bool,
-                Literal::Number(_) => Ty::Int,
-            },
-            Term::Var(name) => self
-                .inner
-                .iter()
-                .find(|bind| bind.name == *name)
-                .ok_or_else(|| TyError::Unbound(Located::new(name.0.to_string(), loc)))?
-                .ty
-                .clone(),
-            Term::Abs(bind, body) => {
-                self.inner.push(bind.clone());
-                let ty = self.type_of(body.as_ref())?.content;
-                self.inner.pop().unwrap();
-                Ty::Arrow(Box::new(bind.ty.clone()), Box::new(ty))
-            }
-            Term::UnaryOp(op, term) => {
-                let ty = self.type_of(term.as_ref())?;
-                match op {
-                    UnOp::Neg => ensure_ty!(Ty::Int, ty)?,
-                    UnOp::Not => ensure_ty!(Ty::Bool, ty)?,
-                }
-            }
+        match &term.content {
+            Term::Lit(lit) => self.type_of_lit(loc, lit),
+            Term::Var(name) => self.type_of_var(loc, name),
+            Term::Abs(bind, body) => self.type_of_abs(loc, bind, body.as_ref()),
+            Term::UnaryOp(op, term) => self.type_of_unary_op(loc, *op, term.as_ref()),
             Term::BinaryOp(op, t1, t2) => {
-                let ty1 = self.type_of(t1.as_ref())?;
-                let ty2 = self.type_of(t2.as_ref())?;
-                match op {
-                    BinOp::Add
-                    | BinOp::Sub
-                    | BinOp::Mul
-                    | BinOp::Div
-                    | BinOp::Rem
-                    | BinOp::BitAnd
-                    | BinOp::BitOr
-                    | BinOp::BitXor
-                    | BinOp::Shr
-                    | BinOp::Shl => ensure_ty!(Ty::Int, ty1, ty2)?,
-                    BinOp::Or | BinOp::And => ensure_ty!(Ty::Bool, ty1, ty2)?,
-                    BinOp::Lt | BinOp::Gt | BinOp::Lte | BinOp::Gte => {
-                        ensure_ty!(Ty::Int, ty1, ty2)?;
-                        Ty::Bool
-                    }
-                    BinOp::Eq | BinOp::Neq => {
-                        ensure_ty!(ty1.content, ty2)?;
-                        Ty::Bool
-                    }
-                }
+                self.type_of_binary_op(loc, *op, t1.as_ref(), t2.as_ref())
             }
-            Term::App(t1, t2) => {
-                if let Term::PrimFn(primitive) = t1.content {
-                    self.type_of_primitive(primitive, t2)
-                } else {
-                    let ty1 = self.type_of(t1.as_ref())?;
-                    let ty2 = self.type_of(t2.as_ref())?;
-                    match ty1.content {
-                        Ty::Arrow(ty11, ty) => {
-                            ensure_ty!(*ty11, ty2)?;
-                            *ty
-                        }
-                        _ => return Err(TyError::ExpectedFn(ty1)),
-                    }
-                }
-            }
-            Term::Let(name, t1, t2) => {
-                let bind = self.type_of(t1.as_ref()).map(|ty| Binding {
-                    name: name.content,
-                    ty: ty.content,
-                })?;
-                self.inner.push(bind);
-                let ty2 = self.type_of(t2.as_ref())?;
-                self.inner.pop().unwrap();
-                ty2.content
-            }
-            Term::Cond(t1, t2, t3) => {
-                let ty1 = self.type_of(t1.as_ref())?;
-                let ty2 = self.type_of(t2.as_ref())?;
-                let ty3 = self.type_of(t3.as_ref())?;
-                ensure_ty!(Ty::Bool, ty1)?;
-                ensure_ty!(ty2.content, ty3)?
-            }
-            Term::Seq(t1, t2) => {
-                let ty1 = self.type_of(t1.as_ref())?;
-                ensure_ty!(Ty::Unit, ty1)?;
-                return self.type_of(t2.as_ref());
-            }
-            Term::Fix(t1) => {
-                let ty = self.type_of(t1.as_ref())?;
-                if let Ty::Arrow(ty1, ty2) = ty.content {
-                    ensure_ty!(*ty1, Located::new(*ty2, ty.loc))?
-                } else {
-                    return Err(TyError::ExpectedFn(ty));
-                }
-            }
+            Term::App(t1, t2) => self.type_of_app(loc, t1.as_ref(), t2.as_ref()),
+            Term::Let(name, t1, t2) => self.type_of_let(loc, name, t1.as_ref(), t2.as_ref()),
+            Term::Cond(t1, t2, t3) => self.type_of_cond(loc, t1.as_ref(), t2.as_ref(), t3.as_ref()),
+            Term::Seq(t1, t2) => self.type_of_seq(loc, t1.as_ref(), t2.as_ref()),
+            Term::Fix(t1) => self.type_of_fix(loc, t1.as_ref()),
             Term::PrimFn(prim) => unreachable!(
                 "Primitives always need special case handling but got {:?}",
                 prim
             ),
+        }
+    }
+
+    fn type_of_lit(&mut self, loc: Location, lit: &Literal) -> TyResult<Located<Ty>> {
+        let ty = match lit {
+            Literal::Unit => Ty::Unit,
+            Literal::Bool(_) => Ty::Bool,
+            Literal::Number(_) => Ty::Int,
         };
         Ok(Located::new(ty, loc))
     }
 
-    fn type_of_primitive(&mut self, primitive: Primitive, _arg: &Located<Term>) -> Ty {
-        match primitive {
-            Primitive::Print => Ty::Unit,
+    fn type_of_var(&mut self, loc: Location, name: &Name<'a>) -> TyResult<Located<Ty>> {
+        let ty = self
+            .inner
+            .iter()
+            .find(|bind| bind.name == *name)
+            .ok_or_else(|| TyError::Unbound(Located::new(name.0.to_string(), loc)))?
+            .ty
+            .clone();
+        Ok(Located::new(ty, loc))
+    }
+
+    fn type_of_abs(
+        &mut self,
+        loc: Location,
+        bind: &Binding<'a>,
+        body: &Located<Term<'a>>,
+    ) -> TyResult<Located<Ty>> {
+        self.inner.push(bind.clone());
+        let ty = self.type_of(body)?.content;
+        self.inner.pop().unwrap();
+        let ty = Ty::Arrow(Box::new(bind.ty.clone()), Box::new(ty));
+        Ok(Located::new(ty, loc))
+    }
+
+    fn type_of_unary_op(
+        &mut self,
+        loc: Location,
+        op: UnOp,
+        term: &Located<Term<'a>>,
+    ) -> TyResult<Located<Ty>> {
+        let ty = self.type_of(term)?;
+        let ty = match op {
+            UnOp::Neg => ensure_ty!(Ty::Int, ty)?,
+            UnOp::Not => ensure_ty!(Ty::Bool, ty)?,
+        };
+        Ok(Located::new(ty, loc))
+    }
+
+    fn type_of_binary_op(
+        &mut self,
+        loc: Location,
+        op: BinOp,
+        t1: &Located<Term<'a>>,
+        t2: &Located<Term<'a>>,
+    ) -> TyResult<Located<Ty>> {
+        let ty1 = self.type_of(t1)?;
+        let ty2 = self.type_of(t2)?;
+        let ty = match op {
+            BinOp::Add
+            | BinOp::Sub
+            | BinOp::Mul
+            | BinOp::Div
+            | BinOp::Rem
+            | BinOp::BitAnd
+            | BinOp::BitOr
+            | BinOp::BitXor
+            | BinOp::Shr
+            | BinOp::Shl => ensure_ty!(Ty::Int, ty1, ty2)?,
+            BinOp::Or | BinOp::And => ensure_ty!(Ty::Bool, ty1, ty2)?,
+            BinOp::Lt | BinOp::Gt | BinOp::Lte | BinOp::Gte => {
+                ensure_ty!(Ty::Int, ty1, ty2)?;
+                Ty::Bool
+            }
+            BinOp::Eq | BinOp::Neq => {
+                ensure_ty!(ty1.content, ty2)?;
+                Ty::Bool
+            }
+        };
+        Ok(Located::new(ty, loc))
+    }
+
+    fn type_of_app(
+        &mut self,
+        loc: Location,
+        t1: &Located<Term<'a>>,
+        t2: &Located<Term<'a>>,
+    ) -> TyResult<Located<Ty>> {
+        if let Term::PrimFn(primitive) = t1.content {
+            self.type_of_primitive(loc, primitive, t2)
+        } else {
+            let ty1 = self.type_of(t1)?;
+            let ty2 = self.type_of(t2)?;
+            match ty1.content {
+                Ty::Arrow(ty11, ty) => {
+                    ensure_ty!(*ty11, ty2)?;
+                    Ok(Located::new(*ty, loc))
+                }
+                _ => Err(TyError::ExpectedFn(ty1)),
+            }
+        }
+    }
+
+    fn type_of_let(
+        &mut self,
+        loc: Location,
+        name: &Located<Name<'a>>,
+        t1: &Located<Term<'a>>,
+        t2: &Located<Term<'a>>,
+    ) -> TyResult<Located<Ty>> {
+        let bind = self.type_of(t1).map(|ty| Binding {
+            name: name.content,
+            ty: ty.content,
+        })?;
+        self.inner.push(bind);
+        let ty2 = self.type_of(t2)?;
+        self.inner.pop().unwrap();
+        Ok(Located::new(ty2.content, loc))
+    }
+
+    fn type_of_cond(
+        &mut self,
+        loc: Location,
+        t1: &Located<Term<'a>>,
+        t2: &Located<Term<'a>>,
+        t3: &Located<Term<'a>>,
+    ) -> TyResult<Located<Ty>> {
+        let ty1 = self.type_of(t1)?;
+        let ty2 = self.type_of(t2)?;
+        let ty3 = self.type_of(t3)?;
+        ensure_ty!(Ty::Bool, ty1)?;
+        let ty = ensure_ty!(ty2.content, ty3)?;
+        Ok(Located::new(ty, loc))
+    }
+
+    fn type_of_seq(
+        &mut self,
+        _loc: Location,
+        t1: &Located<Term<'a>>,
+        t2: &Located<Term<'a>>,
+    ) -> TyResult<Located<Ty>> {
+        let ty1 = self.type_of(t1)?;
+        ensure_ty!(Ty::Unit, ty1)?;
+        self.type_of(t2)
+    }
+
+    fn type_of_fix(&mut self, loc: Location, t1: &Located<Term<'a>>) -> TyResult<Located<Ty>> {
+        let ty = self.type_of(t1)?;
+        if let Ty::Arrow(ty1, ty2) = ty.content {
+            let ty = ensure_ty!(*ty1, Located::new(*ty2, ty.loc))?;
+            Ok(Located::new(ty, loc))
+        } else {
+            Err(TyError::ExpectedFn(ty))
+        }
+    }
+
+    fn type_of_prim_app(
+        &mut self,
+        loc: Location,
+        prim: Primitive,
+        _arg: &Located<Term>,
+    ) -> TyResult<Located<Ty>> {
+        match prim {
+            Primitive::Print => Ok(Located::new(Ty::Unit, loc)),
         }
     }
 }
