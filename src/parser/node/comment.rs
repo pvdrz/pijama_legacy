082342b8295582e5a6c71b7733f376e43ca3e59f
Sean Chen <skypemaster007@gmail.com>
2020-05-23 21:38:52 -0700

    Add comment parser to parser::block

--- src/parser/node/comment.rs
+++ src/parser/node/comment.rs
@@ -4,21 +4,17 @@
 //! Single-line comments are denoted with the `#` character.
 
 use nom::{
-    combinator::map,
+    combinator::value,
     character::complete::{char, line_ending, not_line_ending},
     sequence::delimited,
     multi::many0,
 };
 
 use crate::{
-    ast::{Location, Located, Node, Literal},
     parser::{IResult, Span},
 };
 
-/// Parses a comment, returning a `Literal::Unit`.
-pub fn comment(input: Span) -> IResult<Located<Node>> {
-    map(
-       delimited(char('#'), many0(not_line_ending), line_ending),
-       |_| { Location::new( Literal::Unit, loc )} 
-    )(input)
+/// Parses a comment, returning () if it finds one.
+pub fn comment(input: Span) -> IResult<()> {
+    value((), delimited(char('#'), many0(not_line_ending), line_ending))(input)
 }
