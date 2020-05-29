b823ef87163197e26fd3854c5ae9f91fecd3f47c
Christian Poveda <git@christianpoveda.xyz>
2020-05-29 12:24:26 -0500

    add checked arithmetic

--- src/machine/arithmetic.rs
+++ src/machine/arithmetic.rs
@@ -1,3 +1,5 @@
+use std::convert::TryFrom;
+
 use pijama_ast::{BinOp, BinOp::*, UnOp, UnOp::*};
 
 /// Trait determining how arithmetic operations should be handled.
@@ -36,5 +38,60 @@ impl Arithmetic for OverflowArithmetic {
             Neg => -n,
             Not => !n,
         }
+    }
+}
+
+/// Checked arithmetic that panics when overflowing or dividing by zero.
+pub struct CheckedArithmetic;
+
+impl Arithmetic for CheckedArithmetic {
+    fn native_bin_op(op: BinOp, n1: i64, n2: i64) -> i64 {
+        let (result, overflowed) = match op {
+            Add => n1.overflowing_add(n2),
+            Sub => n1.overflowing_sub(n2),
+            Mul => n1.overflowing_mul(n2),
+            Div => n1.overflowing_div(n2),
+            Rem => n1.overflowing_rem(n2),
+            Lt => ((n1 < n2).into(), false),
+            Lte => ((n1 <= n2).into(), false),
+            Gt => ((n1 > n2).into(), false),
+            Gte => ((n1 >= n2).into(), false),
+            Eq => ((n1 == n2).into(), false),
+            Neq => ((n1 != n2).into(), false),
+            BitAnd | And => (n1 & n2, false),
+            BitOr | Or => (n1 | n2, false),
+            BitXor => (n1 ^ n2, false),
+            Shr => n1.overflowing_shr(try_into_u32_or_panic(n2)),
+            Shl => n1.overflowing_shl(try_into_u32_or_panic(n2)),
+        };
+
+        if overflowed {
+            panic!(
+                "Binary operation `{}` overflowed with operands `{}` and `{}`",
+                op, n1, n2
+            )
+        }
+
+        result
+    }
+
+    fn native_un_op(op: UnOp, n: i64) -> i64 {
+        let (result, overflowed) = match op {
+            Neg => n.overflowing_neg(),
+            Not => (!n, false),
+        };
+
+        if overflowed {
+            panic!("Unary operation `{}` overflowed with operand `{}`", op, n)
+        }
+
+        result
+    }
+}
+
+fn try_into_u32_or_panic(n: i64) -> u32 {
+    match u32::try_from(n) {
+        Ok(n) => n,
+        Err(_) => panic!("Operand `{}` is negative", n),
     }
 }
