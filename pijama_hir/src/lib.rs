7ca0f2fd57383f7b0ae637f17a0b3d203b3f3cbd
Christian Poveda <git@christianpoveda.xyz>
2020-08-09 15:12:22 -0500

    add way to display IRs

--- pijama_hir/src/lib.rs
+++ pijama_hir/src/lib.rs
@@ -1,8 +1,7 @@
-use std::fmt::Debug;
+use std::fmt::{self, Debug};
 
-use pijama_common::{BinOp, Literal, Primitive, UnOp};
-
-use pijama_ctx::{LocalId, TermId};
+use pijama_common::{BinOp, Literal, Local, Primitive, UnOp};
+use pijama_ctx::{Context, LocalId, TermId};
 
 pub use lower::{lower_ast, LowerError, LowerErrorKind, LowerResult};
 
@@ -24,6 +23,10 @@ impl Term {
     pub(crate) fn new(id: TermId, kind: TermKind) -> Self {
         Self { id, kind }
     }
+
+    pub fn show(&self, ctx: &Context) {
+        println!("{}", TermCtx::new(self, ctx))
+    }
 }
 
 #[derive(Debug)]
@@ -37,4 +40,74 @@ pub enum TermKind {
     BinaryOp(BinOp, Box<Term>, Box<Term>),
     Cond(Box<Term>, Box<Term>, Box<Term>),
     Let(BindKind, LocalId, Box<Term>, Box<Term>),
+}
+
+struct TermCtx<'ast, 'ctx> {
+    term: &'ctx Term,
+    ctx: &'ctx Context<'ast>,
+}
+
+impl<'ast, 'ctx> TermCtx<'ast, 'ctx> {
+    fn new(term: &'ctx Term, ctx: &'ctx Context<'ast>) -> Self {
+        Self { term, ctx }
+    }
+
+    fn get_local(&self, id: LocalId) -> Local<'ast> {
+        self.ctx.get_local(id).unwrap()
+    }
+
+    fn spawn(&self, term: &'ctx Term) -> Self {
+        Self::new(term, self.ctx)
+    }
+}
+
+impl<'ast, 'ctx> fmt::Display for TermCtx<'ast, 'ctx> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self.term.kind {
+            TermKind::Lit(lit) => write!(f, "{}", lit),
+            TermKind::PrimFn(prim) => write!(f, "{}", prim),
+            TermKind::Var(local_id) => write!(f, "{}", self.get_local(*local_id)),
+            TermKind::Abs(arg_id, body) => write!(
+                f,
+                "(λ{}. {})",
+                self.get_local(*arg_id),
+                self.spawn(body.as_ref())
+            ),
+            TermKind::App(func, arg) => write!(
+                f,
+                "({} {})",
+                self.spawn(func.as_ref()),
+                self.spawn(arg.as_ref())
+            ),
+            TermKind::UnaryOp(op, term) => write!(f, "({}{})", op, self.spawn(term.as_ref())),
+            TermKind::BinaryOp(op, t1, t2) => write!(
+                f,
+                "({} {} {})",
+                self.spawn(t1.as_ref()),
+                op,
+                self.spawn(t2.as_ref())
+            ),
+            TermKind::Cond(if_term, do_term, else_term) => write!(
+                f,
+                "(if {} then {} else {})",
+                self.spawn(if_term.as_ref()),
+                self.spawn(do_term.as_ref()),
+                self.spawn(else_term.as_ref())
+            ),
+            TermKind::Let(kind, lhs_id, rhs, tail) => {
+                let kind = match kind {
+                    BindKind::NonRec => "",
+                    BindKind::Rec => "rec",
+                };
+                write!(
+                    f,
+                    "(let{} {} = {} in {})",
+                    kind,
+                    self.get_local(*lhs_id),
+                    self.spawn(rhs.as_ref()),
+                    self.spawn(tail.as_ref())
+                )
+            }
+        }
+    }
 }
